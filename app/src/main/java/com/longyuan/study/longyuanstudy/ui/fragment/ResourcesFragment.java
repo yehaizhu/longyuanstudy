03d8d62c95b025d050b25e9cc84d074fcf2b4781
zhuyehai <994>
2017-07-27 20:24:29 +0800

    框架问题修改

--- app/src/main/java/com/longyuan/study/longyuanstudy/ui/fragment/ResourcesFragment.java
+++ app/src/main/java/com/longyuan/study/longyuanstudy/ui/fragment/ResourcesFragment.java
@@ -1,11 +1,7 @@
 package com.longyuan.study.longyuanstudy.ui.fragment;
 
-import android.widget.TextView;
-
 import com.longyuan.study.longyuanstudy.ui.activity.R;
 import com.zhuyehai.base.ui.BaseFragment;
-
-import butterknife.Bind;
 
 /**
  * Created by zhuyehai on 17/7/27.
@@ -13,8 +9,6 @@ import butterknife.Bind;
 
 public class ResourcesFragment extends BaseFragment {
 
-    @Bind(R.id.sss)
-    TextView sss;
 
     @Override
     protected void initData() {
